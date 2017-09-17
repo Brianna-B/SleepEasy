06cf70e93e85abc0cd00cdbc031d660bc4c61143
YUE WU <ywa208@sfu.ca>
2017-09-17 12:43:13 -0700

    log sleep data and update noisePickManager

--- app/src/main/java/com/example/lumoteam/sleepeasy/HomePage.java
+++ app/src/main/java/com/example/lumoteam/sleepeasy/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.lumoteam.sleepeasy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +15,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.util.Log;
 
+import java.util.Date;
+
 public class HomePage extends AppCompatActivity {
     private android.widget.Button play;
     private android.widget.Button log;
@@ -23,14 +27,18 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        soundPlay = new SoundPlay(getApplicationContext());
+        SoundPlay.INSTANCE.init(getApplicationContext());
+        NoisePickManager.INSTANCE.init(getApplicationContext(),15);
         log = (Button) findViewById(R.id.Log);
         play = (Button) findViewById(R.id.Play);
         settings = (Button) findViewById(R.id.Settings);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                soundPlay.play(getApplicationContext());
+                long datetime = new Date().getTime();
+                String key = getApplicationContext().getString(R.string.preference_getIntoBedDate);
+                SharedPreferences datePref = getApplicationContext().getSharedPreferences(key, Context.MODE_PRIVATE);
+                datePref.edit().putLong(key, datetime).apply();
             }
         });
         log.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/lumoteam/sleepeasy/log.java
+++ app/src/main/java/com/example/lumoteam/sleepeasy/log.java
@@ -1,17 +1,92 @@
 package com.example.lumoteam.sleepeasy;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class log extends AppCompatActivity {
+
+    private Calendar sleepCalendar;
+    private Calendar wakeupCalendar;
+    private Calendar outOfBedCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        Button saveLog = (Button) findViewById(R.id.saveLog);
+        final TextView sleepTime = (TextView) findViewById(R.id.sleepTime);
+        sleepTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment fragment = new TimePickerFragment();
+                fragment.setListener(new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        sleepCalendar = Calendar.getInstance();
+                        sleepCalendar.set(Calendar.HOUR, hourOfDay);
+                        sleepCalendar.set(Calendar.MINUTE, minute);
+                        if(hourOfDay < 12) sleepCalendar.add(Calendar.DAY_OF_YEAR, -1);
+                    }
+                });
+                fragment.show(getSupportFragmentManager(), "sleepTime");
+            }
+        });
+        TextView wakeTime = (TextView) findViewById(R.id.wakeTime);
+        wakeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment fragment = new TimePickerFragment();
+                fragment.setListener(new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        wakeupCalendar = Calendar.getInstance();
+                        wakeupCalendar.set(Calendar.HOUR, hourOfDay);
+                        wakeupCalendar.set(Calendar.MINUTE, minute);
+                    }
+                });
+                fragment.show(getSupportFragmentManager(), "wakeTime");
+            }
+        });
+        final TextView outOfBedTime = (TextView) findViewById(R.id.outOfBedTime);
+        outOfBedTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment fragment = new TimePickerFragment();
+                fragment.setListener(new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        outOfBedCalendar = Calendar.getInstance();
+                        outOfBedCalendar.set(Calendar.HOUR, hourOfDay);
+                        outOfBedCalendar.set(Calendar.MINUTE, minute);
+                    }
+                });
+                fragment.show(getSupportFragmentManager(), "outOfBedTime");
+            }
+        });
+        saveLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RatingBar ratingBar = (RatingBar) findViewById(R.id.sleepWell);
+                String key = getApplicationContext().getString(R.string.preference_getIntoBedDate);
+                SharedPreferences datePref = getApplicationContext().getSharedPreferences(key, Context.MODE_PRIVATE);
+                Date intoBedDate = new Date(datePref.getLong(key, 0));
+                NoisePickManager.INSTANCE.update(sleepCalendar.getTime(), intoBedDate, wakeupCalendar.getTime(), new Date(), ratingBar.getNumStars()*20);
+            }
+        });
     }
 }
