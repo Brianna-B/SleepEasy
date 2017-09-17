b44dac5ecb58f044847d36cdeb1b7fb6bf45d0b8
Yan <xuany@f45c89a2e42f.ant.amazon.com>
2017-09-17 12:27:48 -0700

    pause and resume

--- app/src/main/java/com/example/lumoteam/sleepeasy/HomePage.java
+++ app/src/main/java/com/example/lumoteam/sleepeasy/HomePage.java
@@ -2,35 +2,46 @@ package com.example.lumoteam.sleepeasy;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.view.View.OnClickListener;
 
-import android.widget.Button;
-import android.util.Log;
+import java.util.List;
 
 public class HomePage extends AppCompatActivity {
     private android.widget.Button play;
     private android.widget.Button log;
     private android.widget.Button settings;
-    private static SoundPlay soundPlay;
+    private List<Integer> noises;
+    private int isPlaying = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        soundPlay = new SoundPlay(getApplicationContext());
+        SoundPlay.INSTANCE.init(getApplicationContext());
+        NoisePickManager.INSTANCE.init(getApplicationContext(), 15);
         log = (Button) findViewById(R.id.Log);
         play = (Button) findViewById(R.id.Play);
         settings = (Button) findViewById(R.id.Settings);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                soundPlay.play(getApplicationContext());
+                System.out.println(isPlaying);
+                if (isPlaying == 0) {
+                    isPlaying = 1;
+                    noises = NoisePickManager.INSTANCE.pick(5);
+                    System.out.println(noises);
+                    SoundPlay.INSTANCE.play(noises);
+                }
+                else if (isPlaying == 1){
+                    isPlaying = -1;
+                    SoundPlay.INSTANCE.pause();
+                }
+                else {
+                    isPlaying = 1;
+                    SoundPlay.INSTANCE.resume();
+                }
             }
         });
         log.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +64,7 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        soundPlay.stop();
+        SoundPlay.INSTANCE.stop();
         getDelegate().onDestroy();
     }
 }
--- app/src/main/java/com/example/lumoteam/sleepeasy/SoundPlay.java
+++ app/src/main/java/com/example/lumoteam/sleepeasy/SoundPlay.java
@@ -5,62 +5,62 @@ package com.example.lumoteam.sleepeasy;
  */
 
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.content.Context;
 import android.os.Build;
-import android.provider.Settings;
+import java.util.List;
 
 
-public class SoundPlay {
-    private SoundPool sounds;
+public enum SoundPlay {
+    INSTANCE;
 
-    public SoundPlay(Context context) {
+    private SoundPool candidates;
+    private List<Integer> noises;
+
+    public void init(Context context) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            sounds = new SoundPool.Builder()
-                    .setMaxStreams(8)
+            this.candidates = new SoundPool.Builder()
+                    .setMaxStreams(15)
                     .build();
         } else {
-            sounds = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
+            this.candidates = new SoundPool(15, AudioManager.STREAM_MUSIC, 0);
         }
-        sounds.load(context, R.raw.bird, 1);
-        sounds.load(context, R.raw.blastwave, 1);
-        sounds.load(context, R.raw.creek_1, 1);
-        sounds.load(context, R.raw.creek_2, 1);
-        sounds.load(context, R.raw.fire_1, 1);
-        sounds.load(context, R.raw.fire_2, 1);
-        sounds.load(context, R.raw.insects_1, 1);
-        sounds.load(context, R.raw.insects_2, 1);
-        sounds.load(context, R.raw.lake, 1);
-        sounds.load(context, R.raw.ocean, 1);
-        sounds.load(context, R.raw.rain_1, 1);
-        sounds.load(context, R.raw.rain_2, 1);
-        sounds.load(context, R.raw.thunder, 1);
-        sounds.load(context, R.raw.waterfall, 1);
-        sounds.load(context, R.raw.wind, 1);
+        this.candidates.load(context, R.raw.bird, 1);
+        this.candidates.load(context, R.raw.blastwave, 1);
+        this.candidates.load(context, R.raw.creek_1, 1);
+        this.candidates.load(context, R.raw.creek_2, 1);
+        this.candidates.load(context, R.raw.fire_1, 1);
+        this.candidates.load(context, R.raw.fire_2, 1);
+        this.candidates.load(context, R.raw.insects_1, 1);
+        this.candidates.load(context, R.raw.insects_2, 1);
+        this.candidates.load(context, R.raw.lake, 1);
+        this.candidates.load(context, R.raw.ocean, 1);
+        this.candidates.load(context, R.raw.rain_1, 1);
+        this.candidates.load(context, R.raw.rain_2, 1);
+        this.candidates.load(context, R.raw.thunder, 1);
+        this.candidates.load(context, R.raw.waterfall, 1);
+        this.candidates.load(context, R.raw.wind, 1);
     }
 
-    public void play(Context context) {
-        sounds.play(1,1,1,0,-1,1);
-        sounds.play(2,1,1,0,-1,1);
-//        sounds.play(3,1,1,0,-1,1);
-//        sounds.play(4,1,1,0,-1,1);
-        sounds.play(5,1,1,0,-1,1);
-        sounds.play(6,1,1,0,-1,1);
-        sounds.play(7,1,1,0,-1,1);
-        sounds.play(8,1,1,0,-1,1);
-//        sounds.play(9,1,1,0,-1,1);
-//        sounds.play(10,1,1,0,-1,1);
-//        sounds.play(11,1,1,0,-1,1);
-//        sounds.play(12,1,1,0,-1,1);
-        sounds.play(13,1,1,0,-1,1);
-//        sounds.play(14,1,1,0,-1,1);
-        sounds.play(15,1,1,0,-1,1);
+    public void play(List<Integer> noises) {
+        this.noises = noises;
+        for (int i = 0; i < noises.size(); i++) {
+            candidates.play(noises.get(i)+1,1,1,0,-1,1);
+        }
     }
 
     public void stop() {
-        sounds.stop(1);
-        sounds.stop(2);
+        for (int i = 0; i < noises.size(); i++) {
+            candidates.stop(i+1);
+        }
+    }
+
+    public void pause() {
+        candidates.autoPause();
+    }
+
+    public void resume() {
+        candidates.autoResume();
     }
 }
