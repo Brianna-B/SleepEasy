0467e4fdc6987e625dbe08796acd2a82c9efb674
YUE WU <ywa208@sfu.ca>
2017-09-17 10:16:45 -0700

    added NoisePickManager

--- app/src/main/java/com/example/lumoteam/sleepeasy/NoisePickManager.java
+++ app/src/main/java/com/example/lumoteam/sleepeasy/NoisePickManager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -54,14 +55,14 @@ public enum NoisePickManager {
         return pickedNoises;
     }
 
-    public void update(float value) {
+    private void updateWithVal(float value) {
         SharedPreferences trendValuePref = context.getSharedPreferences(context.getString(R.string.preference_trend_value), Context.MODE_PRIVATE);
         Float trendValue = trendValuePref.getFloat(context.getString(R.string.preference_trend_value), 0);
         SharedPreferences lastPickedPref = context.getSharedPreferences(context.getString(R.string.preference_lastpicked), Context.MODE_PRIVATE);
         SharedPreferences scoresPref = context.getSharedPreferences(context.getString(R.string.preference_scores), Context.MODE_PRIVATE);
         SharedPreferences.Editor scoresEditor = scoresPref.edit();
 
-        Float scale = (trendValue - value)/trendValue;
+        Float scale = (value - trendValue)/trendValue;
         Map<String, ?> lastPickedMap = lastPickedPref.getAll();
         for(Map.Entry<String, ?> entry : lastPickedMap.entrySet()){
             String scoresKey = String.valueOf(entry.getValue());
@@ -73,5 +74,13 @@ public enum NoisePickManager {
         scoresEditor.apply();
         trendValue = trendValue * 0.6f + value * 0.4f;
         trendValuePref.edit().putFloat(context.getString(R.string.preference_trend_value), trendValue).apply();
+    }
+
+    public void update(Date fallAsleepTime, Date getIntoBedTime, Date wakeUpTime, Date plannedWakeUpTime, int selfEval) {
+        Float sleepSec = (fallAsleepTime.getTime() - getIntoBedTime.getTime())/1000f;
+        Float score1 = sleepSec < 15*60 ? 100 : 100 - (sleepSec/60 - 15);
+        Float wakeUpSec = (plannedWakeUpTime.getTime() - wakeUpTime.getTime())/1000f;
+        Float score2 = wakeUpSec > 0 ? 100 : 100 + wakeUpSec/60;
+        updateWithVal(score1*0.5f + score2*0.25f + selfEval*0.25f);
     }
 }
